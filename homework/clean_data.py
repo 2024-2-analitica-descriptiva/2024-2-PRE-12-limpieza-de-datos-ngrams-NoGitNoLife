da1bdf5f945f3399ecb76fc4bc10d625412ef9a4
NoGitNoLife <111093017+NoGitNoLife@users.noreply.github.com>
2024-11-29 18:17:17 -0500

    act 2

--- homework/clean_data.py
+++ homework/clean_data.py
@@ -88,7 +88,7 @@ def main(input_file, output_file, n=2):
 
 if __name__ == "__main__":
     main(
-        input_file="files/input/input.txt",
+        input_file="files/input.txt",
         output_file="files/output.txt",
     )
     
